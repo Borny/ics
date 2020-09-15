5ba709c99b0090621df2a6b71210b42d7ec07123
tristan <borntowebb@gmail.com>
2020-09-15 18:58:13 +0200

    Rebase from dev

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,4 @@
 export const environment = {
   production: true,
-<<<<<<< HEAD
   apiUrl: 'https://icsfrance.herokuapp.com/api'
-=======
-  apiUrl: 'https://icsfrance.herokuapp.com'
->>>>>>> Updated the prod api URL
 };
