c41d08396d3137b581ab0c12fc3e7b0c1c8aaf19
Borny <borntowebb@gmail.com>
2021-06-08 15:25:24 +0200

    Changed the atom button hover style && changed the api url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://icsfrance.herokuapp.com/api'
+  // apiUrl: 'https://icsfrance.herokuapp.com/api'
+  apiUrl: 'https://icsfrance2020.herokuapp.com/api'
 };
