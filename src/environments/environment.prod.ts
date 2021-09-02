7a7c3adaa2712110f50850711e1d3e9ab1e24706
Borny <borntowebb@gmail.com>
2021-09-03 00:38:16 +0200

    Prod mode

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  // apiUrl: 'https://icsfrance.herokuapp.com/api'
-  apiUrl: 'https://icsfrance2020.herokuapp.com/api', // Test URL hosted on tristandeloris heorku account
+  apiUrl: 'https://icsfrance.herokuapp.com/api'
+  // apiUrl: 'https://icsfrance2020.herokuapp.com/api', // Test URL hosted on tristandeloris heroku account
 };
